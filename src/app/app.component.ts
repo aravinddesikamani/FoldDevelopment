9f09a7ed58ebb2fa14e86bfc99fe62fd726842ce
root <root@localhost.localdomain>
2023-09-17 22:56:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'This application was developed in samsung galaxy fold!';
 }
